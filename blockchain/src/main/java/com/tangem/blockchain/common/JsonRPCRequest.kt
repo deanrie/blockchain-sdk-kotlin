7ea8fa5ca8f2a472b30485df944d6dd0865d3565
Tangem <git@tangem.com>
2026-07-07 13:25:27 +0300

    Updated on 2026-09-23

--- blockchain/src/main/java/com/tangem/blockchain/common/JsonRPCRequest.kt
+++ blockchain/src/main/java/com/tangem/blockchain/common/JsonRPCRequest.kt
@@ -14,6 +14,6 @@ import java.util.UUID
 internal data class JsonRPCRequest(
     @Json(name = "method") val method: String,
     @Json(name = "params") val params: Any?,
-    @Json(name = "id") val id: String = UUID.randomUUID().toString(),
+    @Json(name = "id") val id: Any = UUID.randomUUID().toString(),
     @Json(name = "jsonrpc") val jsonRpc: String = "2.0",
 )
